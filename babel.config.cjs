5350cb7beba12a2de67280032d0632b3424ef1bc
Jonathan Perry <jonathanperry@modernpoacher.com>
2024-02-04 23:33:03 +0000

    Babel

--- babel.config.cjs
+++ babel.config.cjs
@@ -20,7 +20,8 @@ function env () {
 
 const presets = [
   [
-    '@babel/env', {
+    '@babel/env',
+    {
       targets: {
         node: 'current',
         browsers: [
@@ -38,8 +39,6 @@ const presets = [
 ]
 
 const plugins = [
-  '@babel/proposal-export-default-from',
-  '@babel/proposal-export-namespace-from',
   [
     'module-resolver', {
       alias: {
